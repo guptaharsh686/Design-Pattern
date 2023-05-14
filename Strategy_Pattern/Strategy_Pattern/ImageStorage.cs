dfa23b96b1e3f5bbfb82c7b78c520944776fd3d9
harsh_git <guptaharsh686@gmail.com>
2023-05-14 10:11:35 +0530

    Initial commit for problem

---
+++ Strategy_Pattern/Strategy_Pattern/ImageStorage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy_Pattern
+{
+    public class ImageStorage
+    {
+        private string compressor;
+        private string filter;
+
+        public ImageStorage(string compressor, string filter)
+        {
+            this.compressor = compressor;
+            this.filter = filter;
+        }
+
+        public void store(string fileName)
+        {
+            //JPEG,PNG - Image compression algo
+            if(compressor == "jpeg")
+            {
+                Console.WriteLine("Compressing using JPEG");
+            }
+            else if(compressor == "png")
+            {
+                Console.WriteLine("Compressing using PNG");
+            }
+
+            if(filter == "b&w")
+            {
+                Console.WriteLine("Applying B&w filter");
+            }
+            else if(filter == "high-contrast")
+            {
+                Console.WriteLine("Applying high-contrast filter");
+            }
+        }
+    }
+}
---
+++ Strategy_Pattern/Strategy_Pattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Strategy_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
