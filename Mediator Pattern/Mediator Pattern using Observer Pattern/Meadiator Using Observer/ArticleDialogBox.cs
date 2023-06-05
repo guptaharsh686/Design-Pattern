75afe38a069a5601649a2db1318ac2153dac464f
harsh_git <guptaharsh686@gmail.com>
2023-06-06 00:23:03 +0530

    Meadiator using observer implemented.

--- Mediator Pattern/Mediator Pattern using Observer Pattern/Meadiator Using Observer/ArticleDialogBox.cs
+++ Mediator Pattern/Mediator Pattern using Observer Pattern/Meadiator Using Observer/ArticleDialogBox.cs
@@ -16,9 +16,23 @@ namespace Meadiator_Using_Observer
             this.button = new Button();
             this.listBox = new ListBox();
             this.textBox = new TextBox();
+
+            this.listBox.addObserver(new ConcreteListObserver(this));
+            this.textBox.addObserver(new ConcreteTextBoxObserver(this));
             
         }
-
+        
+        public void simulateInteraction()
+        {
+            listBox.setSelection("Article 1");
+            Console.WriteLine($"TextBox : " + textBox.getContent());
+            Console.WriteLine($"Button : " + button.getEnabled());
+            textBox.setContent("");
+            Console.WriteLine($"Button : " + button.getEnabled());
+            textBox.setContent("Article 2");
+            Console.WriteLine($"TextBox : "+ textBox.getContent());
+            Console.WriteLine($"Button : " + button.getEnabled());
+        }
 
         private void articleSelected()
         {
@@ -28,5 +42,57 @@ namespace Meadiator_Using_Observer
             else
                 button.setEnabled(false);
         }
+
+        private void titleChanged()
+        {
+            var content = textBox.getContent();
+            var isEmpty = string.IsNullOrEmpty(content);
+            button.setEnabled(!isEmpty);
+        }
+
+        public class ConcreteListObserver : IObserver
+        {
+            private ArticleDialogBox dialogBox;
+
+            public ConcreteListObserver(ArticleDialogBox dialogBox)
+            {
+                this.dialogBox = dialogBox;
+            }
+            public override void update()
+            {
+                dialogBox.articleSelected();
+            }
+        }
+
+        public class ConcreteTextBoxObserver : IObserver
+        {
+            private ArticleDialogBox dialogBox;
+
+            public ConcreteTextBoxObserver(ArticleDialogBox dialogBox)
+            {
+                this.dialogBox = dialogBox;
+            }
+            public override void update()
+            {
+                dialogBox.titleChanged();
+            }
+        }
+
+
+        public class ConcreteButtonObserver : IObserver
+        {
+            private ArticleDialogBox dialogBox;
+
+            public ConcreteButtonObserver(ArticleDialogBox dialogBox)
+            {
+                this.dialogBox = dialogBox;
+            }
+            public override void update()
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
+
+    
 }
--- Mediator Pattern/Mediator Pattern using Observer Pattern/Meadiator Using Observer/Program.cs
+++ Mediator Pattern/Mediator Pattern using Observer Pattern/Meadiator Using Observer/Program.cs
@@ -7,6 +7,8 @@ namespace Meadiator_Using_Observer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var article = new ArticleDialogBox();
+            article.simulateInteraction();
         }
     }
 }
