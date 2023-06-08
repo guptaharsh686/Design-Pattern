61c6426fa188a47b18e51692228142019f1bbf08
harsh_git <guptaharsh686@gmail.com>
2023-06-09 00:23:38 +0530

    Modifying the pipeline to add/remove new middlewares.

---
+++ Chain Of Responsibility Pattern/Chain Of Responsibility/Encryption.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chain_Of_Responsibility
+{
+    public class Encryption : Handler
+    {
+        public Encryption(Handler handler) : base(handler)
+        {
+        }
+
+        protected override bool dohandle(HttpRequest httpRequest)
+        {
+            Console.WriteLine("Encryption");
+            return false;
+        }
+    }
+}
--- Chain Of Responsibility Pattern/Chain Of Responsibility/Program.cs
+++ Chain Of Responsibility Pattern/Chain Of Responsibility/Program.cs
@@ -10,13 +10,16 @@ namespace Chain_Of_Responsibility
 
 
             // Authentication -> Compression -> Logging
+            // Authentication -> Compression -> Encryption -> Logging
 
             var logging = new Logging(null);
-            var compression = new Compression(logging);
+            var encryption = new Encryption(logging);
+            var compression = new Compression(encryption);
             var authenticator = new Authentication(compression);
 
             var webServer = new WebServer(authenticator);
             webServer.handle_request(new HttpRequest("admin", "1234"));
+            Console.WriteLine("-----------------------------------------------------");
             webServer.handle_request(new HttpRequest("amit", "1234"));
         }
     }
