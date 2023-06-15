35eb4c70ae5bb8f442de416c885d860dc4266b8f
harsh_git <guptaharsh686@gmail.com>
2023-06-15 17:22:24 +0530

    initial commit

---
+++ Decorator Pattern/Decorator Pattern Exercise/Demo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator_Pattern_Exercise
+{
+    public class Demo
+    {
+        public static void show()
+        {
+            var editor = new Editor();
+            editor.openProject("...");
+        }
+    }
+}
---
+++ Decorator Pattern/Decorator Pattern Exercise/Editor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator_Pattern_Exercise
+{
+    public class Editor
+    {
+        public void openProject(String path)
+        {
+            List<Artefact> artefacts = new List<Artefact>
+            {
+                new Artefact("Main"),
+                new Artefact("Demo"),
+                new Artefact("EmailClient"),
+                new Artefact("EmailProvider"),
+            };
+
+            artefacts[0].setMain(true);
+            artefacts[2].setHasError(true);
+
+            foreach(var artefact in artefacts)
+                Console.WriteLine(artefact.render());
+        }
+    }
+}
