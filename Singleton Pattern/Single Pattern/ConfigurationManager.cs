6c6c18ce119afb8c798e092bd6df7b9a518cc645
harsh_git <guptaharsh686@gmail.com>
2023-06-25 23:24:21 +0530

    Singleton pattern problem

---
+++ Singleton Pattern/Single Pattern/ConfigurationManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Single_Pattern
+{
+    public class ConfigurationManager
+    {
+        private Dictionary<string, Object> settings = new Dictionary<string, object>();
+
+        public void set(string key,Object value)
+        {
+            settings.Add(key, value);
+        }
+
+        public Object get(string key)
+        {
+            Object value;
+            settings.TryGetValue(key, out value);
+            return value;
+        }
+    }
+}
---
+++ Singleton Pattern/Single Pattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Single_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var config = new ConfigurationManager();
+            config.set("wadawd", "adWADadQW");
+
+            Console.WriteLine(config.get("wadawd"));
+
+            // the problem
+            var config1 = new ConfigurationManager();
+            // will give null because this is different instance
+            Console.WriteLine(config1.get("wadawd") == null ? "null" : config1.get("wadawd"));
+
+
+
+        }
+    }
+}
