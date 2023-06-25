51e3c5751f8a23dc4ff11576174b57f20cc81275
harsh_git <guptaharsh686@gmail.com>
2023-06-26 00:40:19 +0530

    My implementation ofsingleton pattern

---
+++ Singleton Pattern/Singleton Pattern Exercise/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton_Pattern_Exercise
+{
+    public class Logger
+    {
+        public string fileName;
+
+
+        private Logger()
+        {
+
+        }
+
+        private static Logger instance = new Logger();
+
+        public static Logger getInstance()
+        {
+            return instance;
+        }
+
+        public void setFileName(string name)
+        {
+            fileName = name;
+        }
+
+
+        public string getFileNAme()
+        {
+            return fileName;
+        }
+
+        public void write(string message)
+        {
+            Console.WriteLine($"Writing a message to the log. Filename = {fileName}");
+        }
+    }
+}
---
+++ Singleton Pattern/Singleton Pattern Exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Singleton_Pattern_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var logger1 = Logger.getInstance();
+            logger1.setFileName("file");
+            logger1.write("msg");
+
+
+            var logger2 = Logger.getInstance();
+            logger2.setFileName("file2");
+            logger2.write("msg2");
+        }
+    }
+}
