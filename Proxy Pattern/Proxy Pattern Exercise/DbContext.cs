6ab9de2482238b9cfe6a85717c6bbb4d5121fd25
harsh_git <guptaharsh686@gmail.com>
2023-06-25 12:11:00 +0530

    Proxy pattern exercise initialization

---
+++ Proxy Pattern/Proxy Pattern Exercise/DbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proxy_Pattern_Exercise
+{
+    public class DbContext
+    {
+        private Dictionary<int, Product> updatedObjects = new Dictionary<int, Product>();
+
+        public Product getProduct(int id)
+        {
+            // Automatically generate SQL statements
+            // to read the product with the given ID.
+            Console.WriteLine($"SELECT * FROM products WHERE product_id = {id} \n");
+
+            // Simulate reading a product object from a database.
+            var product = new Product(id);
+            product.setName("Product 1");
+
+            return product;
+        }
+
+        public void saveChanges()
+        {
+            // Automatically generate SQL statements
+            // to update the database.
+            foreach (var updatedObject in updatedObjects.Values)
+                Console.WriteLine($"UPDATE products SET name = {updatedObject.getName()} WHERE product_id = {updatedObject.getId()} \n");
+
+            updatedObjects.Clear();
+        }
+
+        public void markAsChanged(Product product)
+        {
+            updatedObjects.Add(product.getId(), product);
+        }
+    }
+}
---
+++ Proxy Pattern/Proxy Pattern Exercise/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proxy_Pattern_Exercise
+{
+    public class Product
+    {
+        private int id;
+        private string name;
+
+        public Product(int id)
+        {
+            this.id = id;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+    }
+}
