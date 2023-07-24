89be4979e7e3e55368e803afd883a8dd4637ac02
harsh_git <guptaharsh686@gmail.com>
2023-07-25 01:13:19 +0530

    Added Repository Pattern using Entity Framework

---
+++ Entity-Framework-Patterns/Repository-Pattern/MovieManagementService/MovieManagement.API/Controllers/ActorsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieManagement.Domain.Repository;
+
+namespace MovieManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActorsController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public ActorsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var actorsFromRepo = _unitOfWork.Actor.GetAll();
+            return Ok(actorsFromRepo);
+        }
+
+        [HttpGet("movies")]   
+        public ActionResult GetActorsWithMovies() 
+        {
+            var actorsFromRepo = _unitOfWork.Actor.GetActorsWithMovies();
+            return Ok(actorsFromRepo);
+        }
+    }
+}
